8dada656eaadc7c0480f065a2660186efa1d53c7
billyjovenxd <39799414+billyjovenxd@users.noreply.github.com>
2018-06-07 07:47:03 -0500

    Update proyecto_base.py

--- projects/02 Proyecto II/proyecto_base/proyecto_base.py
+++ projects/02 Proyecto II/proyecto_base/proyecto_base.py
@@ -3,7 +3,16 @@ import instaintro_gui
 
 def tarea():
 	# Aqui empieza tu tarea
-	pass
+	import
+	while(True) :
+    		acción = instaintro_gui.esperar_click()
+    		if accion == "gris" :
+        		imagen = instaintro.gui.obetener pixeles()
+        		for i in imagen :
+            			for j in i :
+                			print(j)
+    	return
+	
 
 
 app = instaintro_gui.Application("tarea")
